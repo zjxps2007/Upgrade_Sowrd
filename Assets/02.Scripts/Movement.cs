fe4373aea7c8b0c48f125de5629937fe1d370107
InHo Jo <zjxps2007@gmail.com>
2025-12-30 18:09:59 +0900

    update

--- Assets/02.Scripts/Movement.cs
+++ Assets/02.Scripts/Movement.cs
@@ -3,35 +3,49 @@ using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    public float moveSpeed = 5f; // Movement speed
+    [SerializeField] private float moveSpeed = 5f;
+    
+    private PlayerInput _playerInput;
+    private InputAction _moveAction;
+    private InputAction _jumpAction; // 예시: 새로운 인풋 추가
 
-    private Vector2 movementInput;
+    private void Awake()
+    {
+        if (TryGetComponent(out _playerInput))
+        {
+            _moveAction = _playerInput.actions["Move"];
+            // _jumpAction = _playerInput.actions["Jump"];
+        }
+    }
 
     private void OnEnable()
     {
-        // Subscribe to the Input System's Move action
-        var playerInput = GetComponent<PlayerInput>();
-        playerInput.onActionTriggered += OnActionTriggered;
+        // 점프와 같은 일회성 액션은 이벤트 방식이 효율적입니다.
+        if (_jumpAction != null) _jumpAction.performed += OnJump;
     }
 
     private void OnDisable()
     {
-        // Unsubscribe from the Input System's Move action
-        var playerInput = GetComponent<PlayerInput>();
-        playerInput.onActionTriggered -= OnActionTriggered;
+        if (_jumpAction != null) _jumpAction.performed -= OnJump;
     }
 
-    private void OnActionTriggered(InputAction.CallbackContext context)
+    private void Update()
     {
-        if (context.action.name == "Move")
-        {
-            movementInput = context.ReadValue<Vector2>();
+        HandleMovement();
+    }
 
-            // Convert 2D input to 3D movement
-            Vector3 movement = new Vector3(movementInput.x, 0, movementInput.y);
+    private void HandleMovement()
+    {
+        Vector2 input = _moveAction?.ReadValue<Vector2>() ?? Vector2.zero;
+        if (input == Vector2.zero) return;
 
-            // Move the Cube GameObject
-            transform.Translate(movement * moveSpeed * Time.deltaTime, Space.World);
-        }
+        Vector3 moveDir = new Vector3(input.x, 0, input.y);
+        transform.Translate(moveDir * (moveSpeed * Time.deltaTime), Space.World);
+    }
+
+    private void OnJump(InputAction.CallbackContext context)
+    {
+        // 점프 로직 처리
+        Debug.Log("Jumped!");
     }
 }
